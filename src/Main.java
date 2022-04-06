c849f527c889fafc268c6bf6e132df3081deb837
spirit2311 <bogzabelin@gmail.com>
2022-04-06 19:59:58 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import controllers.ClientController;
+import models.Client;
+import views.ClientView;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Привет! Работаем над веткой develop");
+        Client model = new Client();
+        ClientView view = new ClientView(model);
+        ClientController controller = new ClientController(model, view);
+        controller.runApp();
     }
 }
