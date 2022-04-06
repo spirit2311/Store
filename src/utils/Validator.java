5f0acac20e99d4c99c96cf012a0120de347ec203
spirit2311 <bogzabelin@gmail.com>
2022-04-06 19:52:00 +0300

    add Validator

---
+++ src/utils/Validator.java
@@ -0,0 +1,54 @@
+package utils;
+
+import java.util.Scanner;
+
+public class Validator {
+
+    public static String validateName(Scanner scanner) {
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+            System.out.print("Пусто! Введите имя клиента: ");
+            str = scanner.nextLine().trim();
+        }
+        return str;
+    }
+
+
+    public static int validateQuantityInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите количество!: ");
+        }
+        int quantity = scanner.nextInt();
+        while (quantity <= 0) {
+            System.out.println("Неверное значение! Введите количество: ");
+            while (!scanner.hasNextInt()) {
+                String str = scanner.next().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.println("Введите количество!: ");
+            }
+            quantity = scanner.nextInt();
+        }
+        return quantity;
+    }
+
+    public static double validatePriceInput(Scanner scanner) {
+        while (!scanner.hasNextDouble()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите цену!: ");
+        }
+        double price = scanner.nextDouble();
+        while (price <= 0) {
+            System.out.print("Неверное значение! Введите цену: ");
+            while (!scanner.hasNextDouble()) {
+                String str = scanner.nextLine().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.print("Введите цену!: ");
+            }
+            price = scanner.nextDouble();
+        }
+        return price;
+    }
+}
